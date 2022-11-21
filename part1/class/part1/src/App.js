78f37ad1fbfcd2fcb8f25b67e61f396b827b52bd
yueshengw <111824664+yueshengw@users.noreply.github.com>
2022-11-21 19:04:52 +0000

    Part 1a class complete

--- part1/class/part1/src/App.js
+++ part1/class/part1/src/App.js
@@ -1,8 +1,38 @@
-const App = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      Greeting app created by <a href='https://github.com/yueshengw' target="_blank">yueshengw</a>
+    </div>
+  )
+}
+
+const App = () => {
+  console.log("Hello from App component");
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  const name = 'Mr. Toliver';
+  const age = 28;
+  return (
+    <>
+      <h1>Greetings</h1>
+      <p>It is {now.toString()}</p>
+      <p>Math</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <Hello name='Tyler' age={10 + 21} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
   )
 }
 
