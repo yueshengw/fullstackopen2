ca838fe0c677ee40d6873d6541b4c57dc46e5417
yueshengw <111824664+yueshengw@users.noreply.github.com>
2022-11-25 18:42:52 +0000

    Init Unicafe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      Hello Unicafe
+    </div>
+  )
+}
+
+export default App
