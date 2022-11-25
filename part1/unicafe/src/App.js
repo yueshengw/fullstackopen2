98102b95bbbfdd78ef517b262ab81bd930205ac1
yueshengw <111824664+yueshengw@users.noreply.github.com>
2022-11-25 19:11:53 +0000

    Exercise 1.6 Complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,60 @@
 import { useState } from 'react'
 
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const Display = ({ score }) => {
+  const [good, neutral, bad] = score;
+
+  return (
+    <>
+      <h2>Statistics</h2>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+    </>
+  )
+}
+
+const Feedback = ({ handleClick }) => {
+  const [incrementGood, incrementNeutral, incrementBad] = handleClick;
+
+  return (
+    <>
+      <h2>Give Feedback</h2>
+      <Button text='Good' handleClick={incrementGood} />
+      <Button text='Neutral' handleClick={incrementNeutral} />
+      <Button text='Bad' handleClick={incrementBad} />
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const incrementGood = () => {
+    console.log('Good +1')
+    setGood(good + 1)
+  }
+
+  const incrementNeutral = () => {
+    console.log('Neutral +1')
+    setNeutral(neutral + 1)
+  }
+
+  const incrementBad = () => {
+    console.log('Bad +1')
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      Hello Unicafe
+      <h1>Unicafe</h1>
+      <Feedback handleClick={[incrementGood, incrementNeutral, incrementBad]} />
+      <Display score={[good, neutral, bad]} />
     </div>
   )
 }
