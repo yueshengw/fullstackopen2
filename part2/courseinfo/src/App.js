2df4e658e9a14da0077d02c0e12d62099016a7be
yueshengw <111824664+yueshengw@users.noreply.github.com>
2022-11-28 00:44:38 +0000

    Exercise 2.4 Complete

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -6,7 +6,7 @@ const Part = ({ part, exercises }) => {
 
 const Header = ({ course }) => {
   return (
-    <h1>{course.name}</h1>
+    <h3>{course.name}</h3>
   )
 }
 
@@ -56,35 +56,56 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <Course course={course} />
+    <>
+      <h1>Web Development Curriculum</h1>
+      {courses.map((course, index) => <Course key={index} course={course} />)}
+    </>
   )
 }
 
