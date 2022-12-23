ea9a8468bfcbcc7c0437941f2b80744e16707b69
yueshengw <fwisvvqj@gmail.com>
2022-12-23 18:12:39 -0500

    Class 2c Complete

--- part2/class/part2/src/index.js
+++ part2/class/part2/src/index.js
@@ -2,13 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import axios from 'axios'
 
-import App from './App (2b)'
+import App from './App (2c)'
 
-const baseURL = 'https://yueshengw-studious-fiesta-x7455pvvpr7h9p4w-3001.preview.app.github.dev/'
-const promise1 = axios.get(baseURL+'notes')
-console.log(promise1)
-const promise2 = axios.get(baseURL+"foobar")
-console.log(promise2)
+const baseURL = 'http://localhost:3001/'
+
+axios
+  .get(baseURL+'notes')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+  })
+
+// axios.get(baseURL+'notes').then(response => {
+//   const notes = response.data
+//   console.log(notes)
+// })
+
+// const promise1 = axios.get(baseURL+'notes')
+// console.log(promise1)
+// promise1.then(response => console.log(response))
+
+// const promise2 = axios.get(baseURL+"foobar")
+// console.log(promise2)
 
 const notes = [
     {
@@ -32,5 +47,5 @@ const notes = [
   ]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App notes={notes} />
+    <App />
 )
