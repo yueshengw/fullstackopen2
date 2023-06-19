8c9290d3322e015c0e62f6d620aeb7d3fe84f6d3
yueshengw <fwisvvqj@gmail.com>
2023-06-19 16:16:04 -0400

    Exercises 3.7 - 3.8 Complete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,13 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
 
-app.use(express.json())
+const app = express();
+app.use(express.json());
+morgan.token('body', function (req, res) {
+  //console.log(JSON.stringify(req.body));
+  return req.body !== {} ? JSON.stringify(req.body) : "{}";
+});
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 let persons = [
     { 
@@ -24,70 +30,70 @@ let persons = [
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
     }
-]
+];
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons)
-})
+    res.json(persons);
+});
 
 app.get("/info", (req, res) => {
-    const appInfo = `Phonebook has info for ${persons.length} people`
-    const timeInfo = new Date()
-    console.log(timeInfo)
-    res.send(`<p>${appInfo}</p><p>${timeInfo}</p>`)
-})
+    const appInfo = `Phonebook has info for ${persons.length} people`;
+    const timeInfo = new Date();
+    console.log(timeInfo);
+    res.send(`<p>${appInfo}</p><p>${timeInfo}</p>`);
+});
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id
-  const person = persons.find(person => person.id.toString() === id)
+  const id = req.params.id;
+  const person = persons.find(person => person.id.toString() === id);
 
   if (person) {
-    res.send(person)
+    res.send(person);
   }
   else {
-    res.statusMessage = "no such id in database"
-    res.status(404).end()
+    res.statusMessage = "no such id in database";
+    res.status(404).end();
   }
-})
+});
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id
-  persons = persons.filter(person => person.id.toString() !== id)
+  const id = req.params.id;
+  persons = persons.filter(person => person.id.toString() !== id);
 
-  res.status(204).end()
-})
+  res.status(204).end();
+});
 
 const nameValidation = name => {
   // name needs to be truthy and unique to be true
-  return name && persons.every(person => person.name !== name)
-}
+  return name && persons.every(person => person.name !== name);
+};
 
 const numberValidation = number => {
   // return boolean not number
-  return Boolean(number)
-}
+  return Boolean(number);
+};
 
 const generateValidId = () => {
-  let id = 1
+  let id = 1;
   // super inefficient
   while (persons.some(person => person.id === id)) {
     // 1, ..., 100
-    id = Math.floor(Math.random() * 100) + 1
+    id = Math.floor(Math.random() * 100) + 1;
   }
-  return id
-}
+  return id;
+};
 
 app.post("/api/persons/", (req, res) => {
-  const id = generateValidId()
-  const body = req.body
+  const id = generateValidId();
+  const body = req.body;
   if (nameValidation(body.name) && nameValidation(body.number)) {
     const person = {
       "id": id,
       "name": body.name || "None",
       "number": body.number || "None"
-    }
-    persons = persons.concat(person)
-    res.json(person)
+    };
+    persons = persons.concat(person);
+    res.json(person);
   }
   else {
     res.status(404).json({
@@ -97,12 +103,12 @@ app.post("/api/persons/", (req, res) => {
       : body.name.length > 1 
       ? "name must be unique"
       : "name is missing"
-    })
+    });
   }
 
-})
+});
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
