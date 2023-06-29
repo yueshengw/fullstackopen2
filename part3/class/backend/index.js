ca70c8a867743bd211650ca818752aa036afc618
yueshengw <fwisvvqj@gmail.com>
2023-06-29 16:31:54 -0400

    Class 3d WIP

--- part3/class/backend/index.js
+++ part3/class/backend/index.js
@@ -137,15 +137,17 @@ const generateId = () => {
     return maxId + 1
 }
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
     const body = request.body
-
-    if (!body.content) {
-        // response.status(400).end()
-        return response.status(400).json({
-            error: "content missing"
-        })
+    if (body.content === undefined) {
+        return response.status(404).json({ error: "content missing"})
     }
+    // if (!body.content) {
+    //     // response.status(400).end()
+    //     return response.status(400).json({
+    //         error: "content missing"
+    //     })
+    // }
 
     const note = new Note({
         content: body.content,
@@ -156,18 +158,26 @@ app.post("/api/notes", (request, response) => {
 
     note.save().then(savedNote => {
         response.json(savedNote)
+    }).catch(error => {
+        console.log(error.name)
+        // response.status(404).json({ error: error.errors.content.kind})
+        next(error)
     })
 })
 
 app.put("/api/notes/:id", (request, response, next) => {
     const body = request.body
+    const { content, important } = request.body
 
     const note = {
         content: body.content,
         important: body.important || false,
     }
 
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+        request.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: "query" })
         .then(updatedNote => {
             response.json(updatedNote)
         }) // without the { new: true } argument, updatedNote will be the original document/note instead of the modified.
@@ -190,7 +200,9 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === "CastError") {
         return response.status(400).send({ error: 'malformatted id' })
     }
-
+    else if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message })
+    }
     next(error) // Express has a default error handler
 }
 
