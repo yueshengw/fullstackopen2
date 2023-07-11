6a878380ad52194a65a7f2150987a6186ceee269
yueshengw <fwisvvqj@gmail.com>
2023-07-11 16:06:40 -0400

    Class 3d Complete

--- part3/class/backend/mongo.js
+++ part3/class/backend/mongo.js
@@ -1,30 +1,30 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 if (process.argv.length < 3) {
-    console.log("give password as argument");
-    process.exit(1);
+	console.log("give password as argument")
+	process.exit(1)
 }
 
 // console.log(process.argv[0]);
 // console.log(process.argv[1]);
 // console.log(process.argv[2]);
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://user1:${password}@p3classbackend.5shiohc.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://user1:${password}@p3classbackend.5shiohc.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url).catch(error => {
-    console.log("connection failed");
-    process.exit(1);
-});
+mongoose.set("strictQuery", false)
+mongoose.connect(url).catch(() => {
+	console.log("connection failed")
+	process.exit(1)
+})
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-});
+	content: String,
+	important: Boolean,
+})
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model("Note", noteSchema)
 
 // const note = new Note({
 //     content: "HTML is easy",
@@ -41,14 +41,14 @@ const Note = mongoose.model("Note", noteSchema);
 // })
 
 Note.find({ important: false }).then(result => {
-    console.log("fetch successful");
-    result.forEach(note => {
-        console.log(note);
-    });
-    mongoose.connection.close();
+	console.log("fetch successful")
+	result.forEach(note => {
+		console.log(note)
+	})
+	mongoose.connection.close()
 }).catch(error => {
-    console.log("fetch failed");
-    console.log(error);
-    mongoose.connection.close();
-    process.exit(1);
-});
+	console.log("fetch failed")
+	console.log(error)
+	mongoose.connection.close()
+	process.exit(1)
+})
