272d6c619069bca79ea9b3ee78de43500163a2ef
yueshengw <fwisvvqj@gmail.com>
2023-07-12 15:11:39 -0400

    Class 4a WIP

---
+++ part4/class/backend/app.js
@@ -0,0 +1,32 @@
+const config = require("./utils/config")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const notesRouter = require("./controllers/notes")
+const middleware = require("./utils/middleware")
+const logger = require("./utils/logger")
+const mongoose = require("mongoose")
+
+mongoose.set("strictQuery", false)
+
+logger.info("Connecting to", config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info("Connected to MongoDB")
+	})
+	.catch(error => {
+		logger.error("Error connecting to MongoDB:", error.message)
+	})
+
+app.use(cors())
+app.use(express.static("build"))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use("/api/notes", notesRouter)
+
+app.use(middleware.unKnownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ part4/class/backend/mongo.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+	console.log("give password as argument")
+	process.exit(1)
+}
+
+// console.log(process.argv[0]);
+// console.log(process.argv[1]);
+// console.log(process.argv[2]);
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://user1:${password}@p3classbackend.5shiohc.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set("strictQuery", false)
+mongoose.connect(url).catch(() => {
+	console.log("connection failed")
+	process.exit(1)
+})
+
+const noteSchema = new mongoose.Schema({
+	content: String,
+	important: Boolean,
+})
+
+const Note = mongoose.model("Note", noteSchema)
+
+// const note = new Note({
+//     content: "HTML is easy",
+//     important: false,
+// });
+
+// note.save().then(result => {
+//     console.log("note saved!");
+//     mongoose.connection.close();
+// }).catch(error => {
+//     console.log("saving failed :(");
+//     console.log(error);
+//     process.exit(1);
+// })
+
+Note.find({ important: false }).then(result => {
+	console.log("fetch successful")
+	result.forEach(note => {
+		console.log(note)
+	})
+	mongoose.connection.close()
+}).catch(error => {
+	console.log("fetch failed")
+	console.log(error)
+	mongoose.connection.close()
+	process.exit(1)
+})
